b7d1110706e3a3dc725cfee4c905124ed2ceb81d
John Doe <madda.athiarahman@gmail.com>
2026-04-26 20:39:31 +0700

    add error handling on analysis form

--- frontend/src/components/form/AnalysisForm.tsx
+++ frontend/src/components/form/AnalysisForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import type { UploadSchema } from "../../schemas/input/UploadSchema";
 import type { UseFormReturn } from "react-hook-form";
 import { CloudDownload } from "lucide-react";
@@ -9,7 +9,8 @@ type Props = {
 };
 
 const AnalysisForm = ({ form, handleSubmit }: Props) => {
-  const [fileName, setFileName] = useState("");
+  const file = form.watch("image");
+
   return (
     <>
       <form
@@ -21,51 +22,70 @@ const AnalysisForm = ({ form, handleSubmit }: Props) => {
           onDragOver={(e) => e.preventDefault()}
           onDrop={(e) => {
             e.preventDefault();
-            const file = e.dataTransfer.files?.[0];
-            if (!file) return;
 
-            form.setValue("image", file, { shouldValidate: true });
-            setFileName(file.name);
+            const droppedFile = e.dataTransfer.files?.[0];
+            if (!droppedFile) return;
+
+            form.setValue("image", droppedFile, {
+              shouldValidate: true,
+              shouldDirty: true,
+              shouldTouch: true,
+            });
           }}
           className="w-full flex flex-col items-center justify-center gap-4"
         >
           <label
             htmlFor="file"
-            className="flex items-center justify-center secondary-button text-bs-m md:text-bs p-4 rounded-md"
+            className="flex items-center justify-center secondary-button text-bs-m md:text-bs p-4 rounded-md cursor-pointer"
           >
             <CloudDownload size={28} strokeWidth={2} />
           </label>
+
           <input
             type="file"
             id="file"
             accept="image/*"
             className="hidden"
             onChange={(e) => {
-              const file = e.target.files?.[0];
-              if (!file) return;
+              const selectedFile = e.target.files?.[0];
+              if (!selectedFile) return;
 
-              form.setValue("image", file, { shouldValidate: true });
-              setFileName(file.name);
+              form.setValue("image", selectedFile, {
+                shouldValidate: true,
+                shouldDirty: true,
+                shouldTouch: true,
+              });
             }}
           />
+
           <div className="flex flex-col items-center justify-center text-center gap-1">
-            <p
-              className={`text-bs-m md:text-bs text-cusdarkgrey ${fileName ? "flex justify-center items-center" : "hidden"}`}
-            >
-              {fileName}
-            </p>
+            {file?.name && (
+              <div className="flex flex-col items-center justify-center">
+                <p className="text-bs-m md:text-bs text-cusdarkgrey">
+                  {file.name}
+                </p>
+                {form.formState.errors.image && (
+                  <p className="text-capt-m md:text-capt text-danger ">
+                    {form.formState.errors.image.message}
+                  </p>
+                )}
+              </div>
+            )}
+
             <p className="text-bd-m md:text-bd text-cusblack font-semibold">
               Seret atau klik untuk unggah gambar
             </p>
+
             <p className="text-bs-m md:text-bs text-cusdarkgrey">
               Format JPEG, PNG, WEBP, dan ukuran maksimal 5 MB
             </p>
           </div>
         </div>
       </form>
+
       <button
         form="analysis-form"
-        className=" primary-button text-bs-m md:text-bs py-2 px-4 rounded-md"
+        className="primary-button text-bs-m md:text-bs py-2 px-4 rounded-md"
       >
         Mulai Deteksi
       </button>
