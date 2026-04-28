25f4bd2fcb06cae942d1453e7401726d6717197f
John Doe <madda.athiarahman@gmail.com>
2026-04-28 14:34:31 +0700

    fixing bug on analyze endpoint api

--- backend/src/routes/analyzeRoutes.js
+++ backend/src/routes/analyzeRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { analyzeImage } from "../controllers/analyzeController.js";
+import { verifyTokenOptional } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", analyzeImage);
+router.post("/", verifyTokenOptional, analyzeImage);
 
 export default router;
