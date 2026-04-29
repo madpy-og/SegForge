81acb981aa6b01207e79746b733836ec67f91122
John Doe <madda.athiarahman@gmail.com>
2026-04-29 19:07:05 +0700

    make historypage protected if the user not login yet

--- frontend/src/pages/History.tsx
+++ frontend/src/pages/History.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { Navigate } from "react-router";
 
 type Props = {
   isAuthenticated: boolean;
 };
 
 const History = ({ isAuthenticated }: Props) => {
-  return (
-    <>
-      <Navbar isAuthenticated={isAuthenticated} />
-      <main></main>
-    </>
-  );
+  if (isAuthenticated === true) {
+    return (
+      <>
+        <Navbar isAuthenticated={isAuthenticated} />
+        <main></main>
+      </>
+    );
+  }
+
+  return <Navigate to="/login" replace />;
 };
 
 export default History;
