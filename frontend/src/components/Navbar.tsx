444f25db9f8f6c98a6be9c3284603da0f3315d73
John Doe <madda.athiarahman@gmail.com>
2026-05-11 17:11:20 +0700

    fix bug on analysis detail

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = ({ isAuthenticated }: Props) => {
     }
   };
   return (
-    <header className="fixed top-0 w-full shadow-[0px_4px_10px_-9px_rgba(0,0,0,0.3)]">
+    <header className="fixed z-50 top-0 w-full shadow-[0px_4px_10px_-9px_rgba(0,0,0,0.3)]">
       <nav className="w-full flex items-center justify-between bg-cuswhite px-4 md:px-10 py-4">
         <Link to="/" className="text-bl-m md:text-bl text-cusblack font-bold">
           SegForge
