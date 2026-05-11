4612e182c6a2a70e1ab52dab3cda18b5833aeac5
John Doe <madda.athiarahman@gmail.com>
2026-05-11 21:53:33 +0700

    styling on navbar component

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = ({ isAuthenticated }: Props) => {
   };
   return (
     <header className="fixed z-50 top-0 w-full shadow-[0px_4px_10px_-9px_rgba(0,0,0,0.3)]">
-      <nav className="w-full flex items-center justify-between bg-cuswhite/50 backdrop-blur-xl px-4 md:px-10 py-4">
+      <nav className="w-full flex items-center justify-between bg-white/50 backdrop-blur-lg px-4 md:px-10 py-4">
         <Link to="/" className="text-bl-m md:text-bl text-cusblack font-bold">
           SegForge
         </Link>
